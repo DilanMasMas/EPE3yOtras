39b0128baf9dcd45b3d1e903b937b0d387b14306
DilanMasMas <dilan.cardenas50324@gmail.com>
2019-06-30 20:43:05 -0400

    firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCF1bUmyHD0yYZTTkZy7Eu4tgvFOxTCQnI",
+    authDomain: "noticias-7ab88.firebaseapp.com",
+    databaseURL: "https://noticias-7ab88.firebaseio.com",
+    projectId: "noticias-7ab88",
+    storageBucket: "noticias-7ab88.appspot.com",
+    messagingSenderId: "298771203570",
+    appId: "1:298771203570:web:4da654aac8f34b89"
+  }
 };
